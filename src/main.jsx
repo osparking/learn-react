95cd3692048a2e7f0307f4fe66a37bc923200c6a
박종범 <jbpark03@gmail.com>
2025-06-08 06:13:48 +0900

    conditional style of each product

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './topics/App2'
+import App3 from './topics/App3'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App3 />
   </StrictMode>,
 )
--- src/topics/App3.jsx
+++ src/topics/App3.jsx
@@ -1,15 +1,19 @@
-import React from "react";
 
 const App3 = () => {
   const products = [
-    { name: "상추", id: 1 },
-    { name: "혁대", id: 2 },
-    { name: "락스", id: 3 },
+    { name: "상추", id: 1, isPerishable: true },
+    { name: "혁대", id: 2, isPerishable: false },
+    { name: "락스", id: 3, isPerishable: false },
   ];
 
-  const items = products.map((product) => (
-    <li key={product.id}>{product.name}</li>
-  ));
+const items = products.map((product) => (
+  <li
+    key={product.id}
+    style={{ color: product.isPerishable ? "red" : "black" }}
+  >
+    {product.name}
+  </li>
+));
 
   return (
     <>
