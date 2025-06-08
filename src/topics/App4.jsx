8d32e0971d882567762df4369a0e5726225ae3f0
박종범 <jbpark03@gmail.com>
2025-06-09 02:15:19 +0900

    function keyword omitted for showAlert func

--- src/topics/App4.jsx
+++ src/topics/App4.jsx
@@ -1,10 +1,12 @@
 const App4 = () => {
+  const showAlert = () => {
+    alert("나는 경보이다");
+  };
+
   return (
     <>
       <h1>사건 응답</h1>
-      <button onClick={() => alert("나는 경보이다")}>
-        클릭하면 경보가 떠요.
-      </button>
+      <button onClick={showAlert}>클릭하면 경보가 떠요.</button>
     </>
   );
 };
