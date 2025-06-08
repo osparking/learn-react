5cae1540a9c046d6053c9e0c8fd37c868d794871
박종범 <jbpark03@gmail.com>
2025-06-09 02:11:35 +0900

    embed function declaration in onClick attr value

---
+++ src/topics/App4.jsx
@@ -0,0 +1,12 @@
+const App4 = () => {
+  return (
+    <>
+      <h1>사건 응답</h1>
+      <button onClick={() => alert("나는 경보이다")}>
+        클릭하면 경보가 떠요.
+      </button>
+    </>
+  );
+};
+
+export default App4;
